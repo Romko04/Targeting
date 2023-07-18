ec00f31f858433af3a8159ec9b9363971b46004e
Roman <kurpelroma@gmail.com>
2023-07-18 16:27:25 +0300

    fixed bugs, add validation

--- telegram.php
+++ telegram.php
@@ -1,28 +1,37 @@
+
 <?php
 
-/* https://api.telegram.org/bot6325500727:AAFaRk3B2ci61rW80CTuzn52FB5Q30htCIs/getUpdates,
-где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+  // Перевірка, чи відправлені обов'язкові поля
+  if (empty($_POST['user_name']) || empty($_POST['user_phone'])) {
+    header('Location: validate.html');
+    exit;
+  }
 
-$name = $_POST['user_name'];
-$phone = $_POST['user_phone'];
-$email = $_POST['user_email'];
-$token = "6325500727:AAFaRk3B2ci61rW80CTuzn52FB5Q30htCIs";
-$chat_id = "-987273048";
-$arr = array(
-  'Имя пользователя: ' => $name,
-  'Телефон: ' => $phone,
-  'Email' => $email
-);
+  $name = $_POST['user_name'];
+  $phone = $_POST['user_phone'];
+  $business = $_POST['user_business'];
 
-foreach($arr as $key => $value) {
-  $txt .= "<b>".$key."</b> ".$value."%0A";
-};
+  $token = "6325500727:AAFaRk3B2ci61rW80CTuzn52FB5Q30htCIs"; // Замініть на свій токен
+  $chat_id = "-987273048";
+  $arr = array(
+    'Ім\'я користувача: ' => $name,
+    'Телефон: ' => $phone,
+    'Бізнес: ' => $business
+  );
 
-$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+  $txt = "";
+  foreach ($arr as $key => $value) {
+    $txt .= "<b>" . $key . "</b> " . $value . "%0A";
+  }
 
-if ($sendToTelegram) {
-  header('Location: thank-you.html');
-} else {
-  echo "Error";
+  // Відправка даних до Telegram
+  $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}", "r");
+
+  if ($sendToTelegram) {
+    header('Location: thank-you.html');
+  } else {
+    echo "Error";
+  }
 }
 ?>
